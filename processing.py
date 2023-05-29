fixed normalization to only depending on X_tr, now 70 percent

processing.py
@@ -5,6 +5,7 @@ import mne
 import torch.nn.functional as F
 import scipy.signal as sig
 from matplotlib.pyplot import specgram
+import matplotlib.pyplot as plt
 
 
 
@@ -87,7 +88,7 @@ def shuffle(X, Y):
 
 n_augment = 50
 
-def get_subject_dataset(subject_id = 1, bandpass_filter=False, normalize=False, excluded=[], augment=False):
+def get_subject_dataset(subject_id = 1, bandpass_filter=False, excluded=[], augment=False):
 	data = get_data(subject_id)
 	time_points, events = get_labels(subject_id)
 
@@ -138,13 +139,6 @@ def get_subject_dataset(subject_id = 1, bandpass_filter=False, normalize=False, 
 	X = X[1:, :, :, :].to(torch.float32)
 	Y = Y[1:]
 
-	if bandpass_filter:
-		X = bandpass_filter_data(X, low_freq, high_freq, fs)
-
-	if normalize:
-		X = normalize_data(X)
-
-
 	X, Y = exclude_categories(X, Y, excluded)
 	print(f'Subject {subject_id}: {X.shape}.')
 
@@ -152,13 +146,13 @@ def get_subject_dataset(subject_id = 1, bandpass_filter=False, normalize=False, 
 
 
 
-def get_split_data(subject_id = 1, bandpass_filter=False, normalize=False, excluded=[], augment=False):
+def get_split_data(subject_id = 1, bandpass_filter=False, excluded=[], augment=False):
 
-	X, Y = get_subject_dataset(subject_id, bandpass_filter=False, normalize=False, excluded=excluded, augment=augment)
+	X, Y = get_subject_dataset(subject_id, excluded=excluded, augment=augment)
 
 	Y = F.one_hot(Y, num_classes=(4 - len(excluded)))
 
-	X, Y = shuffle(X, Y)
+	#X, Y = shuffle(X, Y)
 
 	n1 = int(0.8*X.shape[0])
 	n2 = int(0.9*X.shape[0])
@@ -172,28 +166,24 @@ def get_split_data(subject_id = 1, bandpass_filter=False, normalize=False, exclu
 		X_val = bandpass_filter_data(X_val, low_freq, high_freq, fs)
 		X_te = bandpass_filter_data(X_te, low_freq, high_freq, fs)
 
-	if normalize:
-		X_tr = normalize_data(X_tr)
-		X_val = normalize_data(X_val)
-		X_te = normalize_data(X_te)
 
 	return X_tr.to(torch.float32), Y_tr.to(torch.float32), X_val.to(torch.float32), Y_val.to(torch.float32), X_te.to(torch.float32), Y_te.to(torch.float32)
 
 
 
-def get_broad_data(subject_list, bandpass_filter=False, normalize=False, excluded=[], augment=False):
+def get_broad_data(subject_list, bandpass_filter=False, excluded=[], augment=False):
 
-	X_tr, Y_tr, X_val, Y_val, X_te, Y_te = get_split_data(subject_list[0], bandpass_filter=bandpass_filter, normalize=normalize, excluded=excluded, augment=augment)
+	X_tr, Y_tr, X_val, Y_val, X_te, Y_te = get_split_data(subject_list[0], bandpass_filter=bandpass_filter, excluded=excluded, augment=augment)
 	arr = [X_tr, Y_tr, X_val, Y_val, X_te, Y_te]
 	
 
 	for i in range(1, len(subject_list)):
-		X_tri, Y_tri, X_vali, Y_vali, X_tei, Y_tei = get_split_data(subject_list[i], bandpass_filter=bandpass_filter, normalize=normalize, excluded=excluded, augment=augment)
+		X_tri, Y_tri, X_vali, Y_vali, X_tei, Y_tei = get_split_data(subject_list[i], bandpass_filter=bandpass_filter, excluded=excluded, augment=augment)
 		brr = [X_tri, Y_tri, X_vali, Y_vali, X_tei, Y_tei]
 
 		for j in range(len(arr)):
 			arr[j] = torch.cat((arr[j], brr[j]), 0)
-
+	
 	print(f'Acquired {arr[0].shape[0]} data points from subjects {subject_list}.')
 	return arr
 
@@ -253,11 +243,20 @@ def bandpass_filter_data(data, low, high, fs, order=5):
 	return torch.from_numpy(arr.copy())
 
 
-def normalize_data(data):
-	mu = torch.mean(data, -1, keepdim=True)
-	sigma = torch.std(data, -1, keepdim=True)
+def normalize_data(X_tr, X_val, X_te, other):
 
-	return (data - mu) / sigma
+	N_ch = X_tr.shape[2]
+
+	for j in range(N_ch):
+		
+		mu = torch.mean(X_tr[:, 0, j, :])
+		sigma = torch.std(X_tr[:, 0, j, :])
+
+		X_tr[:, 0, j, :] = (X_tr[:, 0, j, :] - mu) / sigma
+		X_val[:, 0, j, :] = (X_val[:, 0, j, :] - mu) / sigma
+		X_te[:, 0, j, :] = (X_te[:, 0, j, :] - mu) / sigma
+
+	return X_tr, X_val, X_te
 
 
 def exclude_categories(X, Y, excluded):
@@ -271,7 +270,8 @@ def exclude_categories(X, Y, excluded):
 
 # --------------------------------------------------- SPECTROGRAM -------------------------------------------------------------------------
 
+#X_tr, _, X_val, _, X_te, _ = get_split_data(1)
 
-
+#normalize_data(X_tr, X_val, X_te)
 
 

test.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score
 from sklearn import metrics
 import numpy as np
 
@@ -10,7 +10,7 @@ from model import ContinuousTransformer
 
 
 n_classes = 4
-model = ContinuousTransformer(in_channels=1, out_channels=8, d_model=16, nhead=16, dim_feedforward=32, dropout=0.2, n_classes=n_classes)
+model = ContinuousTransformer(in_channels=1, out_channels=8, d_model=16, nhead=8, dim_feedforward=32, dropout=0.2, n_classes=n_classes)
 
 def load_model(model_number, model, epoch):
 	filename = 'models/models_' + str(model_number) + '/model_' + str(model_number) + '_epoch_' + str(epoch)+ '.pth'
@@ -30,14 +30,38 @@ def decode_y(Y):
 
 
 #excluded = []
-def test(model, model_number, epoch, subject, normalize, bandpass_filter, excluded_y):
+def test(model, model_number, epoch, subject, normalize, bandpass_filter, excluded_y, augment):
 
 	print(f'Testing Model {model_number}, Epoch {epoch} on Subject {subject}')
-	X, Y = pro.get_subject_dataset(subject, normalize=normalize, bandpass_filter=bandpass_filter, excluded=excluded_y)
+	'''
+
+	subjects = [1, 2, 3, 5, 6, 7, 8]
+	X_tr, Y_tr, X_val, Y_val, X_te, Y_te = pro.get_broad_data(subject_list=subjects, bandpass_filter=bandpass_filter, excluded=excluded_y, augment=augment)
 	
 
-	model = load_model(model_number, model, epoch)
+	N_ch = X_tr.shape[2]
 
+	for j in range(N_ch):
+		
+		mu = torch.mean(X_tr[:, 0, j, :])
+		sigma = torch.std(X_tr[:, 0, j, :])
+
+		X_9[:, 0, j, :] = (X_9[:, 0, j, :] - mu) / sigma
+		
+	torch.save(X_9, 'X_9_norm.pt')
+	torch.save(Y_9, 'Y_9_norm.pt')
+
+	'''
+	X_9 = torch.load('X_9_norm.pt')
+	Y_9 = torch.load('Y_9_norm.pt')
+	#ix = torch.randint(0, X.shape[0], (300, ))
+
+	#X_9, Y_9, _, _, _, _ = pro.get_broad_data(subject_list=[9], bandpass_filter=bandpass_filter, excluded=excluded_y, augment=augment)
+
+	X = X_9
+	Y = Y_9
+
+	model = load_model(model_number, model, epoch)
 
 	model.eval()
 	with torch.no_grad():
@@ -52,15 +76,18 @@ def test(model, model_number, epoch, subject, normalize, bandpass_filter, exclud
 
 	print(f'Test Loss: {loss}')
 	print(f'Percent Correct: {percent}')
+	
+	if len(Y.shape) > 1:
+		preds = decode_y(preds)
+		Y = decode_y(Y)
 
-
-	decoded_preds = decode_y(preds)
-
-	#print(Y[:10])
-	#print(preds[:10])
+	precision = precision_score(Y, preds, average='macro')
+	recall = recall_score(Y, preds, average='macro')
+	print(f'Precision: {round(precision, 2)}')
+	print(f'Recall: {round(recall, 2)}')
 
 	fig, ax = plt.subplots()
-	cm = confusion_matrix(Y, decoded_preds)
+	cm = confusion_matrix(Y, preds)
 	cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = cm)
 	cm_display.plot()
 	
@@ -68,6 +95,7 @@ def test(model, model_number, epoch, subject, normalize, bandpass_filter, exclud
 	plt.ylabel('Targets')
 	plt.xlabel('Predictions')
 	plt.savefig('charts/charts_' + str(model_number) + '/' + str(model_number) + '_' + str(epoch) + '_confusionmatrix.png')	
+	plt.close()
 	return loss, percent
 
 
@@ -86,9 +114,10 @@ def get_max_accuracy(model, model_number, subject):
 	return min_loss, max_percent
 
 
-for i in range(1000, 7000+1000, 1000):
-	test(model, model_number=3, epoch=i, subject=9, normalize=True, bandpass_filter=False, excluded_y=[])
+#for i in range(1000, 20000+1000, 1000):
+#	test(model, model_number=3, epoch=i, subject=9, normalize=True, bandpass_filter=False, excluded_y=[])
 
+test(model, model_number=5, epoch=4000, subject=9, normalize=True, bandpass_filter=False, excluded_y=[], augment=False)
 
 
 

train.py
@@ -19,19 +19,19 @@ from model import ContinuousTransformer
 # 	Play with batch_size, d_model, nhead, dim_ffwd, dropout, optimizer
 
 
-model_number = 3
+model_number = 4
 learning_rate = 0.00002
 validate_fraction = 100
 
-normalize = True
+normalize = False
 bandpass_filter = False
-augment = True
+augment = False
 
 document = True
-print_training_data = False
+print_training_data = True
 
-num_epochs = 10000
-batch_size = 32
+num_epochs = 8000
+batch_size = 64
 
 optimizer_name = 'adam'
 
@@ -42,7 +42,7 @@ excluded_y = []
 
 
 print('Building Transformer Model...')
-model = ContinuousTransformer(in_channels=1, out_channels=8, d_model=16, nhead=16, dim_feedforward=32, dropout=0.2, n_classes=4)
+model = ContinuousTransformer(in_channels=1, out_channels=8, d_model=16, nhead=8, dim_feedforward=32, dropout=0.2, n_classes=4)
 
 
 def get_data(s, bandpass_filter, normalize, excluded_y, augment):
@@ -51,7 +51,7 @@ def get_data(s, bandpass_filter, normalize, excluded_y, augment):
 	print(f'Normalizing: {normalize}.')
 	print(f'Augmenting: {augment}.')
 	
-	X_tr, Y_tr, X_val, Y_val, X_te, Y_te = pro.get_broad_data(subject_list=s, bandpass_filter=bandpass_filter, normalize=normalize, excluded=excluded_y, augment=augment)
+	X_tr, Y_tr, X_val, Y_val, X_te, Y_te = pro.get_broad_data(subject_list=s, bandpass_filter=bandpass_filter, excluded=excluded_y, augment=augment)
 
 
 	print(f'Excluding Y categories {excluded_y}...')
@@ -60,13 +60,19 @@ def get_data(s, bandpass_filter, normalize, excluded_y, augment):
 	X_te, Y_te = pro.exclude_categories(X_te, Y_te, excluded_y)
 
 
-	#X_tr, Y_tr, X_val, Y_val, X_te, Y_te = pro.get_split_data(subject_id=1)
 	print('Input Shape: ', X_tr.shape)
 
-	X_tr, Y_tr = pro.shuffle(X_tr, Y_tr)
-	X_val, Y_val = pro.shuffle(X_val, Y_val)
-	X_te, Y_te = pro.shuffle(X_te, Y_te)
+	#X_tr, Y_tr = pro.shuffle(X_tr, Y_tr)
+	#X_val, Y_val = pro.shuffle(X_val, Y_val)
+	#X_te, Y_te = pro.shuffle(X_te, Y_te)
 
+	print(X_tr.shape, X_val.shape, X_te.shape)
+
+	if normalize:
+		X_tr, X_val, X_te = pro.normalize_data(X_tr, X_val, X_te)
+
+	print(X_tr.shape, X_val.shape, X_te.shape)
+	
 	return X_tr, Y_tr, X_val, Y_val, X_te, Y_te
 
 
@@ -117,26 +123,34 @@ def train(model, model_number, num_epochs, batch_size, optimizer_name, learning_
 	print('Creating Notes File...')
 	if document: doc.create_notes_file(model_number, model, learning_rate, normalize, bandpass_filter, num_epochs, batch_size, excluded_y)
 
-	save = True
+	save = False
 
 	if save:
-		print('Creating Dataset...')
+
 		X_tr, Y_tr, X_val, Y_val, X_te, Y_te = get_data(subjects, bandpass_filter, normalize, excluded_y, augment)
 
+		print('Creating Dataset...')
+		
 		torch.save(X_tr, 'X_tr.pt')
 		torch.save(Y_tr, 'Y_tr.pt')
 		torch.save(X_val, 'X_val.pt')
 		torch.save(Y_val, 'Y_val.pt')
+		torch.save(X_te, 'X_te.pt')
+		torch.save(Y_te, 'Y_te.pt')
 
-	else:
-		X_tr = torch.load('X_tr.pt')
-		Y_tr = torch.load('Y_tr.pt')
-		X_val = torch.load('X_val.pt')
-		Y_val = torch.load('Y_val.pt')
+	X_tr = torch.load('X_tr.pt')
+	Y_tr = torch.load('Y_tr.pt')
+	X_val = torch.load('X_val.pt')
+	Y_val = torch.load('Y_val.pt')
+	X_te = torch.load('X_te.pt')
+	Y_te = torch.load('Y_te.pt')
 
-		X_val = X_val[:int(len(X_val) / 10)]
-		Y_val = Y_val[:int(len(Y_val) / 10)]
-		assert(len(X_val) == len(Y_val))
+	X_val = X_val[:int(len(X_val) / 5)]
+	Y_val = Y_val[:int(len(Y_val) / 5)]
+	assert(len(X_val) == len(Y_val))
+
+	X_tr, Y_tr, X_val, Y_val, X_te, Y_te = get_data(subjects, bandpass_filter, normalize, excluded_y, augment)
+
 
 	if optimizer_name == 'adam':
 		optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -151,14 +165,14 @@ def train(model, model_number, num_epochs, batch_size, optimizer_name, learning_
 
 		
 		
-		if epoch == 0 or (epoch + 1) % (int(num_epochs / validate_fraction)) == 0:
+		if epoch == 0 or (epoch + 1) % validate_fraction == 0:
 			print(f"[Epoch {epoch + 1} / {num_epochs}]")
 
 			model.eval()
 			with torch.no_grad():
 
 				if print_training_data:
-					ix = torch.randint(0, X_tr.shape[0], (batch_size*3, ))
+					ix = torch.randint(0, X_tr.shape[0], (batch_size*5, ))
 					Xb, Yb = X_tr[ix], Y_tr[ix]
 					train_preds = model(Xb)
 
@@ -211,6 +225,13 @@ def train(model, model_number, num_epochs, batch_size, optimizer_name, learning_
 
 
 train(model, model_number, num_epochs, batch_size, optimizer_name, learning_rate, subjects, bandpass_filter, normalize, excluded_y, document=True, print_training_data=True, validate_fraction=100, save_charts=True)
+train(model, 5, num_epochs, batch_size, optimizer_name, learning_rate, subjects, bandpass_filter, True, excluded_y, document=True, print_training_data=True, validate_fraction=100, save_charts=True)
+#train(model, 6, num_epochs, batch_size, optimizer_name, learning_rate, subjects, bandpass_filter, True, excluded_y, document=True, print_training_data=True, validate_fraction=100, save_charts=True)
+#train(model, 7, num_epochs, batch_size, optimizer_name, learning_rate, subjects, bandpass_filter, True, excluded_y, document=True, print_training_data=True, validate_fraction=100, save_charts=True)
+
+
+
+
 #torch.save(model.state_dict(), 'models/models_' + str(model_number) + '/model_1.pth')
 
 '''